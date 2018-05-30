7e6bd58334207a66a381952951279c227163692e
vuru <suleyman.vurucu@sms-lsn.com>
2018-05-30 23:13:22 +0200

    Refactor to fluent api

--- micrometer-core/src/main/java/io/micrometer/core/instrument/Point.java
+++ micrometer-core/src/main/java/io/micrometer/core/instrument/Point.java
@@ -3,8 +3,10 @@ package io.micrometer.core.instrument;
 import io.micrometer.core.instrument.config.NamingConvention;
 
 import java.math.BigInteger;
-import java.time.Duration;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -12,51 +14,45 @@ import java.util.concurrent.TimeUnit;
  * Date: 26.05.2018
  * Time: 20:12
  */
-public class Point  {
-    private final String measurement;
-    private final Map<String, String> tags;
+public class Point {
+
+    private String measurement;
+    private Map<String, String> tags;
+    private Map<String, Object> fields;
     private Long time;
-    private final Map<String, Object> fields;
 
-    public Point(final String measurement) {
+    Point() {
+    }
+
+    public static Builder builder(){
+        return builder(null);
+    }
+
+    public static Builder builder(String measurement){
+        return new Point().new Builder().measurement(measurement);
+    }
+
+
+
+    private Point time(final long timeToSet, TimeUnit pressission) {
+        this.time = getPrecision().convert(timeToSet, pressission);
+        return this;
+    }
+
+
+    private Point measurement(String measurement) {
         this.measurement = measurement;
-        tags = new HashMap<>();
-        fields = new HashMap<>();
-        time(System.currentTimeMillis(),TimeUnit.MILLISECONDS) ;
-    }
-
-    public Point time(final long timeToSet,TimeUnit pressission) {
-        this.time = getPrecision().convert(timeToSet,pressission);
         return this;
     }
 
-    public Point cloneMeasurement() {
-        return new Point(this.measurement);
-    }
 
-    public Point tag(final String tagName, final String value) {
-        Objects.requireNonNull(tagName, "tagName");
-        Objects.requireNonNull(value, "value");
-        if (!tagName.isEmpty() && !value.isEmpty()) {
-            tags.put(tagName, value);
-        }
+    private Point tags(Map<String, String> tags) {
+        this.tags = tags;
         return this;
     }
-    public Point field(final String field, Object value) {
-        if (value instanceof Number) {
-            if (value instanceof Byte) {
-                value = ((Byte) value).doubleValue();
-            } else if (value instanceof Short) {
-                value = ((Short) value).doubleValue();
-            } else if (value instanceof Integer) {
-                value = ((Integer) value).doubleValue();
-            } else if (value instanceof Long) {
-                value = ((Long) value).doubleValue();
-            } else if (value instanceof BigInteger) {
-                value = ((BigInteger) value).doubleValue();
-            }
-        }
-        fields.put(field, value);
+
+    private Point fields(Map<String, Object> fields) {
+        this.fields = fields;
         return this;
     }
 
@@ -97,14 +93,8 @@ public class Point  {
         return builder.toString();
     }
 
-
-
     public Long getTime() {
         return time;
-    }
-
-    public void setTime(Long time) {
-        this.time = time;
     }
 
     public String getMeasurement(MeterRegistry registry) {
@@ -114,26 +104,91 @@ public class Point  {
 
     public Map<String, String> getTags(MeterRegistry registry) {
         final NamingConvention namingConvention = registry.config().namingConvention();
-        Map<String,String> result = new HashMap<>() ;
+        Map<String, String> result = new HashMap<>();
         final Set<Tag> commonTags = registry.config().getCommonTags();
-       commonTags.forEach( tag ->{
-           result.put(namingConvention.tagKey(tag.getKey()),namingConvention.tagValue(tag.getValue()));
-       });
-        tags.forEach((k,v)->{
-            result.put(namingConvention.tagKey(k),namingConvention.tagValue(v));
+        commonTags.forEach(tag -> {
+            result.put(namingConvention.tagKey(tag.getKey()), namingConvention.tagValue(tag.getValue()));
+        });
+        tags.forEach((k, v) -> {
+            result.put(namingConvention.tagKey(k), namingConvention.tagValue(v));
         });
         return result;
     }
+
     public Map<String, Object> getFields(MeterRegistry registry) {
         final NamingConvention namingConvention = registry.config().namingConvention();
-        Map<String,Object> result = new HashMap<>() ;
-        fields.forEach((k,v)->{
-            result.put(namingConvention.tagKey(k),v);
+        Map<String, Object> result = new HashMap<>();
+        fields.forEach((k, v) -> {
+            result.put(namingConvention.tagKey(k), v);
         });
         return result;
     }
 
     public TimeUnit getPrecision() {
         return TimeUnit.NANOSECONDS;
+    }
+
+    public class Builder {
+
+        private String measurement;
+        private Map<String, String> tags;
+        private Map<String, Object> fields;
+
+        private Builder() {
+            tags = new HashMap<>() ;
+            fields = new HashMap<>();
+        }
+
+        public Builder measurement(String measurement) {
+            this.measurement = measurement;
+            return this;
+        }
+
+        public Builder reset() {
+            resetFields();
+            resetTags();
+            return this;
+        }
+
+        public Builder resetTags() {
+            tags = new HashMap<>() ;
+            return this;
+        }
+
+        public Builder resetFields() {
+            fields = new HashMap<>();
+            return this;
+        }
+
+        public Builder tag(final String tagName, final String value) {
+            Objects.requireNonNull(tagName, "tagName");
+            Objects.requireNonNull(value, "value");
+            if (!tagName.isEmpty() && !value.isEmpty()) {
+                tags.put(tagName, value);
+            }
+            return this;
+        }
+
+        public Builder field(final String field, Object value) {
+            if (value instanceof Number) {
+                if (value instanceof Byte) {
+                    value = ((Byte) value).doubleValue();
+                } else if (value instanceof Short) {
+                    value = ((Short) value).doubleValue();
+                } else if (value instanceof Integer) {
+                    value = ((Integer) value).doubleValue();
+                } else if (value instanceof Long) {
+                    value = ((Long) value).doubleValue();
+                } else if (value instanceof BigInteger) {
+                    value = ((BigInteger) value).doubleValue();
+                }
+            }
+            fields.put(field, value);
+            return this;
+        }
+
+        public Point build() {
+            return new Point().measurement(measurement).fields(fields).tags(tags).time(System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+        }
     }
 }
